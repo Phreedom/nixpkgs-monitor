565648ee7ebaecc284dde281bbcb55e6a2760d39
Pascal Wittmann <mail@pascal-wittmann.de>
2015-10-28 16:18:50 +0100

    use new ruby infrastructure, fixes #11
    create a Gemfile with the necessary dependencies,
    use bundix to create gemset.nix and adapt default.nix
    accordingly

--- default.nix
+++ default.nix
@@ -1,39 +1,25 @@
-{nix_src ? null}:
+{ pkgs ? (import <nixpkgs> {}), stdenv ? pkgs.stdenv }:
+
 let
-
-  pkgs = import <nixpkgs> {
-    config = {
-      gems.generated = import ./gems.nix;
-      gems.patches = { gems, postgresql }: {
-        pg = {
-          buildInputs = [ postgresql ];
-        };
-      };
-    };
-  };
-
-  required_gems = with pkgs.rubyLibs; [ mechanize sequel sqlite3 sinatra haml diffy pg ];
-  tame_nix = pkgs.lib.overrideDerivation pkgs.nixUnstable (a: {
-      patches = [ ./expose-attrs.patch ./extra-meta.patch ];
-    });
-
   updater_runtime_deps = with pkgs; [ ruby_1_9 git patch curl bzip2 gnutar gnugrep coreutils gnused bash file ];
 
-in
-with pkgs;
-stdenv.mkDerivation {
+in stdenv.mkDerivation rec {
   name = "nixpkgs-monitor-dev";
 
   src = ./.;
 
-  buildInputs = [ rubygems ruby makeWrapper ];
+  env = pkgs.bundlerEnv {
+    name = "nixpkgs-monitor-dev";
+    ruby = pkgs.ruby_1_9;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
 
-  requiredUserEnvPkgs = required_gems;
-  propagatedBuildInputs = required_gems;
+  buildInputs = [ pkgs.makeWrapper env.ruby pkgs.bundler ];
 
   installPhase = ''
-    addToSearchPath GEM_PATH $out/${ruby.gemPath}
-
+    addToSearchPath GEM_PATH $out/${env.ruby.gemPath}
     export gemlibpath=$out/lib/
     mkdir -p $gemlibpath
     cp distro-package.rb $gemlibpath
@@ -42,22 +28,20 @@ stdenv.mkDerivation {
     cp reports.rb $gemlibpath
     cp build-log.rb $gemlibpath
     cp -r migrations $gemlibpath
-
     mkdir -p $out/bin
     cp updatetool.rb $out/bin
     cp nixpkgs-monitor-site $out/bin
-
     wrapProgram "$out/bin/updatetool.rb" \
-          ${stdenv.lib.concatMapStrings (x: "--prefix PATH : ${x}/bin ") updater_runtime_deps} \
-          --prefix PATH : ${tame_nix}/bin \
-          --prefix GEM_PATH : "$GEM_PATH" \
-          --prefix RUBYLIB : "${rubygems}/lib:$gemlibpath" \
+        ${stdenv.lib.concatMapStrings (x: "--prefix PATH : ${x}/bin ") updater_runtime_deps} \
+          --prefix PATH : "${env.ruby}/bin" \
+          --prefix GEM_PATH : "${env}/${env.ruby.gemPath}" \
+          --prefix RUBYLIB : "${env}/${env.ruby.gemPath}:$out/lib"  \
           --set RUBYOPT rubygems
 
     wrapProgram "$out/bin/nixpkgs-monitor-site" \
-          --set PATH "${ruby_1_9}/bin:${diffutils}/bin:${which}/bin" \
-          --prefix GEM_PATH : "$GEM_PATH" \
-          --prefix RUBYLIB : "${rubygems}/lib:$gemlibpath" \
+          --set PATH "${env.ruby}/bin:${pkgs.diffutils}/bin:${pkgs.which}/bin" \
+          --prefix GEM_PATH : "${env}/${env.ruby.gemPath}" \
+          --prefix RUBYLIB : "${env}/${env.ruby.gemPath}:$out/lib"  \
           --set RUBYOPT rubygems
 
   '';
