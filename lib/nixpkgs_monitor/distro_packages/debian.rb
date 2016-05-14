d1ea67c9d0b835628b01376fc808c87e8f97685a
Phreedom <phreedom@yandex.ru>
2016-05-14 08:46:31 +0300

    nixpkgs-monitor: distro_packages: debian: clean up; don't use a temporary file

--- lib/nixpkgs_monitor/distro_packages/debian.rb
+++ lib/nixpkgs_monitor/distro_packages/debian.rb
@@ -16,16 +16,13 @@ module NixPkgsMonitor module DistroPackages
       %x(curl http://ftp.debian.org/debian/dists/sid/main/source/Sources.xz -o debian-main.xz)
       %x(curl http://ftp.debian.org/debian/dists/sid/contrib/source/Sources.xz -o debian-contrib.xz)
       %x(curl http://ftp.debian.org/debian/dists/sid/non-free/source/Sources.xz -o debian-non-free.xz)
-      %x(xzcat debian-main.xz debian-contrib.xz debian-non-free.xz >debian-sources)
 
-      File.read('debian-sources').split("\n\n").each do |pkgmeta|
-        pkg_name = $1 if pkgmeta =~ /Package:\s*(.*)/
-        pkg_version = $1 if pkgmeta =~ /Version:\s*(.*)/
-        if pkg_name and pkg_version
-          package = Debian.new(pkg_name, pkg_name, pkg_version)
-          deb_list[package.name] = package if package
-        end
-      end
+      %x(xzcat debian-main.xz debian-contrib.xz debian-non-free.xz)
+          .split("\n\n").each do |pkgmeta|
+            next unless (/Package:\s*(?<pkg_name>.+)/ =~ pkgmeta and
+                         /Version:\s*(?<pkg_version>.+)/ =~ pkgmeta)
+            deb_list[pkg_name] = Debian.new(pkg_name, pkg_name, pkg_version)
+          end
 
       serialize_list(deb_list.values)
     end
